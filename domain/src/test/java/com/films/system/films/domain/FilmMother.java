024a1f33a7b69716a70e29883b51114481d98138
ivan.betanzos <ivan.betanzos89@gmail.com>
2023-11-22 16:22:35 +0100

    fix FilmMother not to have an Application layer dependency

--- domain/src/test/java/com/films/system/films/domain/FilmMother.java
+++ domain/src/test/java/com/films/system/films/domain/FilmMother.java
@@ -1,7 +1,6 @@
 package com.films.system.films.domain;
 
 import com.films.system.common.domain.valueobject.FilmId;
-import com.films.system.films.application.dto.FilmCreateDto;
 import com.films.system.films.domain.models.Film;
 import com.films.system.films.domain.valueobjects.FilmLaunchDate;
 import com.films.system.films.domain.valueobjects.FilmTitle;
@@ -16,14 +15,6 @@ public class FilmMother {
         .title(FilmTitleMother.random())
         .launchDate(FilmLaunchDateMother.random())
         .imageId(ImageIdMother.random())
-        .build();
-  }
-
-  public static Film fromRequest(FilmCreateDto requestDto) {
-    return Film.builder()
-        .id(FilmIdMother.from(requestDto.getId().toString()))
-        .title(FilmTitleMother.from(requestDto.getTitle()))
-        .launchDate(FilmLaunchDateMother.from(requestDto.getLaunchDate()))
         .build();
   }
 
